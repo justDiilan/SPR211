c7d32dbae7b8ea7eb38b953ff9ab244ccde6cd58
Daniil <75314872+justDiilan@users.noreply.github.com>
2023-11-12 15:37:32 +0200

    Add files via upload

---
+++ Elf/Elf/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Elven_Mystic;
+
+namespace Elf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ElvenWizard elvenWizard = new ElvenWizard("Elven Wizard", 100, 100, "Staff");
+            elvenWizard.Attack();
+            elvenWizard.Speak();
+            elvenWizard.Stats();
+
+            ElementalSummoner summoner = new ElementalSummoner("Elemental Summoner", 120, 90, "Elementary particles");
+            summoner.Attack();
+            summoner.Speak();
+            summoner.Stats();
+
+            ElementalMaster elementalMaster = new ElementalMaster("Elemental Master", 80, 80, "Magic book");
+            elementalMaster.Attack();
+            elementalMaster.Speak();
+            elementalMaster.Stats();
+
+            SpellSinger spellSinger = new SpellSinger("Spell Singer", 70, 120, "Voice");
+            spellSinger.Attack();
+            spellSinger.Speak();
+            spellSinger.Stats();
+
+            MysticMuse mysticMuse = new MysticMuse("Mystic Muse", 70, 100, "Harp");
+            mysticMuse.Attack();
+            mysticMuse.Speak();
+            mysticMuse.Stats();
+
+            ElvenOracle elvenOracle = new ElvenOracle("Elven Oracle", 60, 50, "Alchemy");
+            elvenOracle.Attack();
+            elvenOracle.Speak();
+            elvenOracle.Stats();
+
+            ElvenElder elvenElder = new ElvenElder("Elven Elder", 80, 50, "Magic scrolls");
+            elvenElder.Attack();
+            elvenElder.Speak();
+            elvenElder.Stats();
+
+            EvasSaint evasSaint = new EvasSaint("Elven Saint", 100, 60, "Prayer");
+            evasSaint.Attack();
+            evasSaint.Speak();
+            evasSaint.Stats();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Elf/Elven Mystic/ElvenMystic.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elven_Mystic
+{
+    public class ElvenMystic
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Karma { get; set; }
+        public string Weapon { get; set; }
+
+        public ElvenMystic(string name, int health, int karma, string weapon)
+        {
+            Name = name;
+            Health = health;
+            Karma = karma;
+            Weapon = weapon;
+        }
+
+        public virtual void Attack()
+        {
+            Console.WriteLine($"{Name} attacks!");
+        }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public virtual void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+
+    public class ElvenOracle : ElvenMystic
+    {
+        public ElvenOracle(string name, int health, int karma, string weapon) : base(name, health, karma, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} uses divine powers!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public override void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+
+    public class ElvenElder : ElvenOracle
+    {
+        public ElvenElder(string name, int health, int karma, string weapon) : base(name, health, karma, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} channels ancient elven energy!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public override void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+
+    public class EvasSaint : ElvenElder
+    {
+        public EvasSaint(string name, int health, int karma, string weapon) : base(name, health, karma, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} invokes the blessings of Evas!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public override void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+
+    public class ElvenWizard : ElvenMystic
+    {
+        public ElvenWizard(string name, int health, int karma, string weapon) : base(name, health, karma, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} casts powerful wizard spells!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public override void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+
+    public class ElementalSummoner : ElvenWizard
+    {
+        public ElementalSummoner(string name, int health, int karma, string weapon) : base(name, health, karma, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} summons elemental forces!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public override void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+
+    public class SpellSinger : ElvenWizard
+    {
+        public SpellSinger(string name, int health, int karma, string weapon) : base(name, health, karma, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} sings magical incantations!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public override void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+
+    public class ElementalMaster : ElementalSummoner
+    {
+        public ElementalMaster(string name, int health, int karma, string weapon) : base(name, health, karma, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} commands mastery over elements!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public override void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+
+    public class MysticMuse : SpellSinger
+    {
+        public MysticMuse(string name, int health, int karma, string weapon) : base(name, health, karma, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} becomes a muse of mystical melodies!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} speaks!");
+        }
+        public override void Stats()
+        {
+            Console.WriteLine($"{Name} use {Weapon}, it has {Karma} karma point and {Health} health point!");
+        }
+    }
+}
