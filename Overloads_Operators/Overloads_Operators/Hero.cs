856ee1704e4a3d9c7cf3418d10ac43714b1ec7b4
Daniil <75314872+justDiilan@users.noreply.github.com>
2023-11-19 17:41:27 +0200

    Add files via upload

---
+++ Overloads_Operators/Overloads_Operators/Hero.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloads_Operators
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+
+        public Hero(string name, int strength)
+        {
+            Name = name;
+            Strength = strength;
+        }
+
+        //Перевантаження оператора додавання
+        public static Hero operator +(Hero hero1, Hero hero2)
+        {
+            return new Hero($"{hero1.Name} and {hero2.Name}", hero1.Strength + hero2.Strength);
+        }
+
+        //Перевантаження оператора віднімання
+        public static Hero operator -(Hero hero, int value)
+        {
+            return new Hero(hero.Name, hero.Strength - value);
+        }
+
+        //Перевантаження оператора множення
+        public static Hero operator *(Hero hero, int value)
+        {
+            return new Hero(hero.Name, hero.Strength * value);
+        }
+
+        //Перевантаження оператора ділення
+        public static Hero operator /(Hero hero, int value)
+        {
+            if(value != 0)
+                return new Hero(hero.Name, hero.Strength / value);
+            else
+                throw new DivideByZeroException("Cannot divide by zero.");
+        }
+
+        //Перевантаження оператора порівняння на рівність
+        public static bool operator ==(Hero hero1, Hero hero2)
+        {
+            return hero1.Strength == hero2.Strength;
+        }
+
+        //Перевантаження оператора порівняння на не рівність
+        public static bool operator !=(Hero hero1, Hero hero2)
+        {
+            return hero1.Strength != hero2.Strength;
+        }
+
+        //Перевантаження оператора більше ніж
+        public static bool operator >(Hero hero1, Hero hero2)
+        {
+            return hero1.Strength > hero2.Strength;
+        }
+
+        //Перевантаження оператора меньше ніж
+        public static bool operator <(Hero hero1, Hero hero2)
+        {
+            return hero1.Strength < hero2.Strength;
+        }
+    }
+}
---
+++ Overloads_Operators/Overloads_Operators/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloads_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero1 = new Hero("Hero 1", 100);
+            Hero hero2 = new Hero("Hero 2", 150);
+
+            //Додавання сил героїв
+            Hero heroCombine = hero1 + hero2;
+            Console.WriteLine($"Combine Hero: {heroCombine.Name}, Strength: {heroCombine.Strength}");
+
+            //Віднімання (ослаблення) героя
+            Hero weakeningHero = hero1 - 50;
+            Console.WriteLine($"Weakening hero: {weakeningHero.Name}, Strength: {weakeningHero.Strength}");
+
+            //Множення (збільшення сил) героя
+            Hero gainHero = hero1 * 2;
+            Console.WriteLine($"Gain hero: {gainHero.Name}, Strength: {gainHero.Strength}");
+
+            //Ділення (ослабення) героя
+            Hero weakeningHeroX2 = hero2 / 50;
+            Console.WriteLine($"Weakening hero X2: {weakeningHeroX2.Name}, Strength: {weakeningHeroX2.Strength}");
+
+            //Порівняння сил героїв
+            Console.WriteLine($"{hero1.Name} == {hero2.Name}: {hero1 == hero2}");
+            Console.WriteLine($"{hero1.Name} != {hero2.Name}: {hero1 != hero2}");
+            Console.WriteLine($"{hero1.Name} > {hero2.Name}: {hero1 > hero2}");
+            Console.WriteLine($"{hero1.Name} < {hero2.Name}: {hero1 < hero2}");
+
+            Console.ReadKey();
+        }
+    }
+}
