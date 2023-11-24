17ab298a261ec8e4a2c020f11bef60210cc9b986
Daniil <75314872+justDiilan@users.noreply.github.com>
2023-11-24 21:44:00 +0200

    Add files via upload

---
+++ Bank_Opearations/Bank/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public int Balance { get; set; }
+        public BankAccount(string accountNumber, int balance) 
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        public delegate void BankOperationHandler(object sender, BankOperationEventArgs e);
+        public event BankOperationHandler BankOperation;
+
+        public void PerformOperation(int amount, string operationType)
+        {
+            if (operationType == "Deposit")
+            {
+                Balance += amount;
+            }
+            else if (operationType == "Withdraw" && amount <= Balance)
+            {
+                Balance -= amount;
+            }
+
+            OnBankOperation(new BankOperationEventArgs(AccountNumber, amount, operationType));
+        }
+
+        public virtual void OnBankOperation(BankOperationEventArgs e)
+        {
+            BankOperation?.Invoke(this, e);
+        }
+    }
+
+    public class BankOperationEventArgs : EventArgs 
+    {
+        public string AccountNumber { get; set; }
+        public int Amount { get; set; }
+        public string OperationType { get; set; }
+        public BankOperationEventArgs(string accountNumber, int amount, string operationType)
+        {
+            AccountNumber = accountNumber;
+            Amount = amount;
+            OperationType = operationType;
+        }
+    }
+
+    public class TransactionManager
+    {
+        public static void Deposit(BankAccount account, int amount)
+        {
+            account.PerformOperation(amount, "Deposit");
+        }
+        public static void Withdraw(BankAccount account, int amount)
+        {
+            account.PerformOperation(amount, "Withdraw");
+        }
+    }
+}
---
+++ Bank_Opearations/Bank_Opearations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Bank;
+
+namespace Bank_Opearations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount("111111", 1000);
+            BankAccount account2 = new BankAccount("222222", 500);
+
+            account1.BankOperation += HandleBankOperation;
+            account2.BankOperation += HandleBankOperation;
+
+            TransactionManager.Deposit(account1, 200);
+            TransactionManager.Withdraw(account2, 100);
+
+            Console.WriteLine($"Account 1 balance: {account1.Balance}");
+            Console.WriteLine($"Account 2 balance: {account2.Balance}");
+
+            Console.ReadKey();
+        }
+
+        static void HandleBankOperation(object sender, BankOperationEventArgs e)
+        {
+            Console.WriteLine($"Account Number: {e.AccountNumber}, Amount: {e.Amount}, Operation Type: {e.OperationType}");
+        }
+    }
+}
