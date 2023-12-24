d7b902985f93712910293fdc6368fa2ecabb9d99
Daniil <75314872+justDiilan@users.noreply.github.com>
2023-12-24 11:08:15 +0200

    Add files via upload

---
+++ GenericsStack/GenericsStack/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericsStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine("Top element: " + stack.Peek());
+
+            int popItem = stack.Pop();
+            Console.WriteLine("Pop element: " + popItem);
+
+            Console.WriteLine("Current count: " + stack.Count);
+
+            stack.Clear();
+            Console.WriteLine("After clear, count: " + stack.Count);
+        }
+    }
+}
---
+++ GenericsStack/GenericsStack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsStack
+{
+    public class Stack<T>
+    {
+        private List<T> elements;
+
+        public Stack() 
+        { 
+            elements = new List<T>();
+        }
+
+        public int Count { get { return elements.Count; } }
+
+        public void Push(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException("Item cannot be null.");
+
+            elements.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (elements.Count == 0)
+                throw new ArgumentNullException("The stack is empty.");
+
+            int lastIndex = elements.Count - 1;
+            T item = elements[lastIndex];
+            elements.RemoveAt(lastIndex);
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (elements.Count == 0)
+                throw new InvalidOperationException("The stack is empty.");
+
+            return elements[elements.Count - 1];
+        }
+
+        public void Clear()
+        {
+            elements.Clear();
+        }
+    }
+}
