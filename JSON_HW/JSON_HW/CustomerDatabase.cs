4c1e273413acf8573a05f5680857fa3adaeda467
Daniil <75314872+justDiilan@users.noreply.github.com>
2024-02-16 20:13:46 +0200

    Add files via upload

---
+++ JSON_HW/JSON_HW/CustomerDatabase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JSON_HW
+{
+    public class CustomerDatabase
+    {
+        private List<Customer> customers;
+        private string filePath;
+        public CustomerDatabase(string filePath)
+        {
+            this.filePath = filePath;
+            if (!File.Exists(filePath)) 
+            {
+                File.WriteAllText(filePath, "[]");
+            }
+
+            string json = File.ReadAllText(filePath);
+            customers = JsonSerializer.Deserialize<List<Customer>>(json);
+        }
+
+        public void SaveChanges()
+        {
+            string json = JsonSerializer.Serialize(customers);
+            File.WriteAllText(filePath, json);
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            if (customers.Any(c => c.Id == customer.Id))
+            {
+                Console.WriteLine($"Customer with identificator {customer.Id}, already exists.");
+            }
+            else
+            {
+                customers.Add(customer);
+            }
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            int index = customers.FindIndex(c => c.Id == customer.Id);
+            if (index != -1)
+            {
+                customers[index] = customer;
+            }
+            else
+            {
+                throw new ArgumentException("Customer not found");
+            }
+        }
+
+        public void DeleteCustomer(int customerId)
+        {
+            Customer customer = customers.Find(c => c.Id == customerId);
+            if (customer != null)
+            {
+                customers.Remove(customer);
+            }
+            else
+            {
+                throw new ArgumentException("Customer not found");
+            }
+        }
+
+        public Customer GetCustomer(int customerId)
+        {
+            return customers.Find(c => c.Id == customerId);
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return customers;
+        }
+    }
+}
---
+++ JSON_HW/JSON_HW/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace JSON_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Learning\C# OOP Academ\JSON_HW\JSON_HW\customers.json";
+            CustomerDatabase database = new CustomerDatabase(filePath);
+
+            // Додати нового клієнта
+            Customer newCustomer1 = new Customer
+            {
+                Id = 1,
+                Name = "Felix Felixovich",
+                Email = "felix@gmail.com",
+                PhoneNumber = "1234567890",
+            };
+            Customer newCustomer2 = new Customer
+            {
+                Id = 2,
+                Name = "John Afrodit",
+                Email = "john@gmail.com",
+                PhoneNumber = "0987654321",
+            };
+            Customer newCustomer3 = new Customer
+            {
+                Id = 3,
+                Name = "Oleksandr Silver",
+                Email = "oleksandr@gmail.com",
+                PhoneNumber = "1234560987",
+            };
+            database.AddCustomer(newCustomer1);
+            database.AddCustomer(newCustomer2);
+            database.AddCustomer(newCustomer3);
+            database.SaveChanges();
+
+            // Оновити існуючого клієнта
+            Customer existingCustomer = database.GetCustomer(1);
+            existingCustomer.PhoneNumber = "098-765-4321";
+            database.UpdateCustomer(existingCustomer);
+            database.SaveChanges();
+
+            // Отримати всіх клієнтів
+            List<Customer> allCustomers = database.GetAllCustomers();
+            foreach (var customer in allCustomers)
+            {
+                Console.WriteLine($"Id: {customer.Id}, Name: {customer.Name}, Email: {customer.Email}, Phone: {customer.PhoneNumber}");
+            }
+
+            // Видалити клієнта
+            database.DeleteCustomer(1);
+            database.SaveChanges();
+
+            // Отримати всіх клієнтівy
+            foreach (var customer in allCustomers)
+            {
+                Console.WriteLine($"Id: {customer.Id}, Name: {customer.Name}, Email: {customer.Email}, Phone: {customer.PhoneNumber}");
+            }
+        }
+    }
+}
